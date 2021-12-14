4d5050ade378a46c087b9b7dcbadd37a9015c731
Alberto De Jesus <alberto.dejesus@upr.edu>
2021-12-14 00:50:09 -0500

    Updated data types

--- cuda_cryptography.cu
+++ cuda_cryptography.cu
@@ -15,45 +15,46 @@ void print(char* content, int size) {
     printf("\n");
 }
 
-void encrypt(RijnKeyParam key, char* content, int size) {
+void encrypt(const RijnKeyParam* key, char** content, int size) {
     for(int idx = 0; idx < size; idx++) {
         rijn_encrypt(key, content[idx]);
     }
 }
 
-void decrypt(RijnKeyParam key, char* content, int size) {
+void decrypt(const RijnKeyParam* key, char** content, int size) {
     for(int idx = 0; idx < size; idx++) {
         rijn_decrypt(key, content[idx]);
     }
 }
 
-RijnKeyParam key() {
-    RijnKeyParam key;
+const RijnKeyParam* key() {
+    const RijnKeyParam* key;
     key.num_key = 4;
     key.num_round = 10;
-    key.enc_key = { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
+    key.enc_key[] = { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
 	16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
     };
-    key.dec_key = { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
+    key.dec_key[] = { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
 	16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
     };
 
     return key;
 }
 
-char* load_file_contents(char* filepath) {
+char** load_file_contents(char* filepath) {
+
     FILE *fp;
     fp = fopen(filepath, "r");
 
     fseek(fp, 0L, SEEK_END);
 
-    char* contents = (char*) malloc(sizeof(char) * ftell(fp));
+    char** contents = (char**) malloc(sizeof(char*) * ftell(fp));
 
     rewind(fp);
 
     int count = 0;
 
-    while(fscanf(file, "%d", &contents[count]) == 1) { 
+    while(fscanf(file, "%s", &contents[count]) == 1) { 
         count++;
     } 
 
@@ -73,25 +74,25 @@ int main(int argc, char** argv) {
 	int numBlocks = 4;
    	int blockSize = 256;
 
-	char* file = "";
+	char* file;
 
 	// read command line arguments
 	if (argc >= 1) {
         usage(argv[0]);
     } else if (argc <= 2) {
-        file = atoi(argv[1]);
+        file = argv[1];
 	} else if (argc <= 3) {
-        file = atoi(argv[1]);
+        file = argv[1];
 		numBlocks = atoi(argv[2]);
 	} else {
         usage(argv[0]);
     }
 
-    int totalThreads = numBlocks * blockSize;
+    // int totalThreads = numBlocks * blockSize;
 
-    char* file_content = load_file_contents(file);
+    char** file_content = load_file_contents(file);
 
-    RijnKeyParam* key = key();
+    const RijnKeyParam* key = key();
 
     print(file_content);
 
